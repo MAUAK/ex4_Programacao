126fa7109a784d1b6b9cb5ed8a292626e2e4c5a1
MAUAK <kauamsilva360@gmail.com>
2021-09-14 23:49:51 -0300

    comentarios

--- ex4_POO/ex4_POO/Calculadora.cs
+++ ex4_POO/ex4_POO/Calculadora.cs
@@ -6,14 +6,19 @@ namespace ex4_POO
 {
     class Calculadora
     {
+        //criando a variavel publica PI
         public double Pi= 3.14;
 
+        //Criando o método Circuferencia e passando como parametro a variavel double r
         public double Circuferencia(double r)
         {
+            //Fazendo o calculo para retornar o valor do comprimenro da circuferencia
             return 2.0 * Pi * r;
         }
+        //Criando o método Volume e passando como parametro a variavel double r
         public double Volume(double r)
         {
+            //Fazendo o calculo para retornar o valor do volume da circuferencia
             return 4.0 / 3.0 * Pi * r * r * r;
         }
     }
