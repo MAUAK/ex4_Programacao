179c791978a02cc540c4bc7aa4f2d2537f66c29f
MAUAK <kauamsilva360@gmail.com>
2021-09-15 23:09:19 -0300

    att

--- ex4_POO/ex4_POO/Program.cs
+++ ex4_POO/ex4_POO/Program.cs
@@ -6,34 +6,14 @@ namespace ex4_POO
     class Program
     {
         //criando a variavel static PI
-        //static double Pi= 3.14;
+        static double Pi= 3.14;
         static void Main(string[] args)
         {
-            //oi
-            //instanciando variavel
-            Calculadora calc = new Calculadora();
-            
-            //Pedindo o valor do raio
-            Console.Write("Entre o valor do raio: ");
-            //Lendo o valor do raio como double
-            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            
-            //criando a varial circ do tipo double e instanciando ela com o método circuferencia da classe calculadora
-            double circ = calc.Circuferencia(raio);
-            //criando a varial volume do tipo double e instanciando ela com o método volume da classe calculadora
-            double volume = calc.Volume(raio);
-            
-            //Escrevendo o resultado da circunferencia e convertendo ela para string
-            Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
-            //Escrevendo o resultado do volume e convertendo ela para string
-            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            //Escrevendo o valor de PI e convertendo ele para string
-            Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
             //---------------------------------------------------
 
             //Pedindo o valor do raio
-            /*Console.WriteLine("Entre com valor do raio: ");
+            Console.WriteLine("Entre com valor do raio: ");
              
             //Lendo o que o usuario digital e guardando na variavel raio
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
@@ -48,10 +28,10 @@ namespace ex4_POO
             //Escrevendo o resultado do volume e convertendo ela para string
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
             //Escrevendo o valor de PI e convertendo ele para string
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));*/
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
         //Criando o método Circuferencia e passando como parametro a variavel double r
-        /*static double Circuferencia(double r) 
+        static double Circuferencia(double r) 
         { 
             //Fazendo o calculo para retornar o valor do comprimenro da circuferencia
             return 2.0 * Pi * r; 
@@ -61,6 +41,6 @@ namespace ex4_POO
         { 
             //Fazendo o calculo para retornar o valor do volume da circuferencia
             return 4.0 / 3.0 * Pi * r * r * r; 
-        }*/
+        }
     }
 }
